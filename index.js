a141e72eb1afbe15e9797202e5dd66f23d6d0227
Özlem Keleş <ozlemkeles98@gmail.com>
2023-03-27 03:05:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+const findMatching = function(drivers, string){
+    return drivers.filter(driver =>
+      driver.toLowerCase() === string.toLowerCase()
+    );
+  }
+
+  const fuzzyMatch = function(drivers, string){
+    return drivers.filter(driver =>
+      driver.toLowerCase().startsWith(string.toLowerCase())
+    );
+  }
+
+  const matchName = function(drivers, string){
+    return drivers.filter(driver =>
+      driver.name.toLowerCase() === string.toLowerCase()
+    );
+  }
